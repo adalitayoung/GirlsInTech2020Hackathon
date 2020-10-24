b0c23dede4c1f584354afb0b02840e182e2624a7
Tiffany Duong <tiff4ny.dev@gmail.com>
2020-10-24 17:00:21 +1100

    added meditation selector to storyboard

---
+++ SmilingMindAppleWatch/ios/Smiling Mind + Body Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Smiling Mind + Body Extension
+//
+//  Created by Tiffany Duong on 24/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmilingMindAppleWatch/ios/Smiling Mind + Body Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Smiling Mind + Body Extension
+//
+//  Created by Tiffany Duong on 24/10/20.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
